6be3d3a867cfc45e6db9c0f122497e33bbd999ee
jotavare <jpoliveira.mm@gmail.com>
2024-05-14 19:56:15 +0100

    added exit/error and additional flags functions

--- kilo.c
+++ kilo.c
@@ -1,39 +1,76 @@
+// INCLUDES
+#include <ctype.h>
+#include <errno.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
 
+// DATA
 struct termios g_original_termios;
 
+void die(const char *s){
+    perror(s);
+    exit(1);
+}
+
+// TERMINAL
 /*
- * restore terminal attributes to original state + flush any inputed values
-*/
+ * Restore the terminal attributes to original state;
+ * Flush any inputed values;
+ */
 void disableRawMode()
 {
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &g_original_termios);
+    if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &g_original_termios) == -1)
+        die("tcsetattr");
 }
 
 /*
- * get the original state of terminal attributes
- * disable ECHO (print to console) and ISIG flags (ctrl+z and ctrl+c)
-*/
+ * Get the original state of terminal attributes;
+ * Disable, change and/or fix flags (description in 'man termios');
+ * iflag and cflag are not necessary but were used in the old days (tradition);
+ * set minimum number of bytes of input;
+ * vtime is in the tenths of a second 1/10
+ */
 void enableRawMode()
 {
-    tcgetattr(STDIN_FILENO, &g_original_termios);
+    if(tcgetattr(STDIN_FILENO, &g_original_termios) == -1)
+        die("tcgetattr");
+    
     atexit(disableRawMode);
 
     struct termios raw = g_original_termios;
 
-    raw.c_lflag &= ~(ECHO);
-    raw.c_lflag &= ~(ISIG);
+    raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
+    raw.c_oflag &= ~(OPOST);
+    raw.c_cflag |= (CS8);
+    raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+    raw.c_cc[VMIN] = 0;
+    raw.c_cc[VTIME] = 1;
 
     tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
 
+// INIT
 int main()
 {
     enableRawMode();
 
-    char c;
-    while(read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
+    while (1)
+    {
+        char c = '\0';
+        if(read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN)
+            die("read");
+
+        if (iscntrl(c))
+        {
+            printf("%d\r\n", c);
+        }
+        else
+        {
+            printf("%d ('%c')\r\n", c, c);
+        }
+        if(c == 'q') break;
+    }
     return 0;
 }
