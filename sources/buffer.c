bb334e99583e565477a4b252ae33abb067d8c269
João Oliveira <38820667+jotavare@users.noreply.github.com>
2024-05-18 20:00:00 +0100

    feat: add file i/o, editing, search and syntax highlighting

--- sources/buffer.c
+++ sources/buffer.c
@@ -1,25 +1,17 @@
 #include "../includes/library.h"
 
-/*
- * Append a string to the buffer, if the buffer is full, reallocate memory;
- * Copy the string to the buffer and update the length of the buffer;
- */
-void abAppend(struct appendBuffer *ab, const char *s, int len)
+void abAppend(struct abuf *ab, const char *s, int len)
 {
     char *new = realloc(ab->b, ab->len + len);
 
     if (new == NULL)
         return;
-
     memcpy(&new[ab->len], s, len);
     ab->b = new;
     ab->len += len;
 }
 
-/*
- * Free the buffer;
- */
-void abFree(struct appendBuffer *ab)
+void abFree(struct abuf *ab)
 {
     free(ab->b);
 }
